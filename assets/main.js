c63e8f0f5b362f6f6412ffcd919aa1a6f176cd80
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-17 16:45:10 +0200

    add: result on page

--- assets/main.js
+++ assets/main.js
@@ -4,20 +4,29 @@
 Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
-let listItemEl = document.querySelector(".listItem");
-let listEl = document.querySelector("ul");
-
+let listEl = document.querySelector(".list");
 
 const emailList = [];
 for (let i = 1; i <= 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            let email = response.data.response;
-            console.log(email);
+            email = response.data.response;
+            //console.log(email);
             emailList.push(email);
+            listEl.innerHTML += `<li> ${email} </li>`;
         })
+        
 }
 
 console.log(emailList);
+
+
+
+
+
+//
+
+
+
 
 
 
