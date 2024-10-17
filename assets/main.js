9bbfc258a56e8445ea1e204c5b0505f30fcbd0df
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-17 15:00:13 +0200

    add: email on page

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,18 @@
 //console.log("Funzioni?");
+
+/* 
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+let listItemEl = document.querySelector(".listItem");
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+  .then(response => {
+    console.log(response);
+    const email = response.data.response;
+    console.log(email);
+    listItemEl.innerHTML = email;
+    });
+
+    
+
